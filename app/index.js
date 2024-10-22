d9f668537d40bd2216b73a89ee27ae94c2c73dfd
bghayadh <bilmar_gh@yahoo.com>
2024-10-23 02:43:19 +0300

    First Commit.

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { Link, Stack } from 'expo-router';
+import { Image, Text, View, StyleSheet } from 'react-native';
+
+export default function Home() {
+    return (
+        <View>
+            <Text>Welcome To SooqNet Mobile App!!!</Text>
+        </View>
+    );
+}
