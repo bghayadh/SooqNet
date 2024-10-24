c7d42d88d2db44be3ffd4f68b59878f3b6591c9d
bghayadh <bilmar_gh@yahoo.com>
2024-10-24 13:47:29 +0300

    Axios

--- app/index.js
+++ app/index.js
@@ -1,10 +1,47 @@
 import { Link, Stack } from 'expo-router';
 import { Image, Text, View, StyleSheet } from 'react-native';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
-    return (
+  const [data, setData] = useState(''); 
+  const [loading, setLoading] = useState(true);
+  const itemCode = 'MEN-SBP-SHO-2024-18';
+  const newItemName = '123Trainers';
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.post('http://192.168.238.237:8080/osc/AxiosTest',{}, {
+          params: {
+            itemCode: itemCode,
+            newItemName: newItemName
+          }
+        });
+        if (response && response.data) {
+          setData(response.data);
+        } 
+        else {
+          setData('No data received.');
+        }
+      } catch (error) {
+        console.error("There was an error!", error.message);
+        setData('Error fetching data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData(); 
+  }, []); 
+
+  return (
         <View>
             <Text>To Zeinab from Bilal, Welcome To SooqNet Mobile App!!!</Text>
+            {loading ? <Text>Loading...</Text> : <Text>{data}</Text>}
         </View>
     );
 }
